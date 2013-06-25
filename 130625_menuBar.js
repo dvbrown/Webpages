1a979c64dd1330c0e80e4b7c5ed1c590aca50622
dvbrown <dvbrown@student.unimelb.edu.au>
2013-06-25 17:24:33 +1000

    build a menu bar to switch protocp;s in javascript

---
+++ 130625_menuBar.js
@@ -0,0 +1,11 @@
+/*
+Dan Brown used javascript because he couldn't be bothered to write a PHP script and set up a server.
+This script prints a menu on the top of protocol pages. The style NAVMENU is defined in plain.css
+*/
+
+document.write('<div id="navmenu"> \
+<ul> \
+	<li><a href="130606_RNAextraction.html">RNA Extraction</a></li> \
+	<li><a href="130606_RNAseqProtocol.html">RNA library prep</a></li> \
+	<li><a href="130521_RNaseEtOHppt.html">Ethanol precipitation</a></li> \
+</ul></div>');
