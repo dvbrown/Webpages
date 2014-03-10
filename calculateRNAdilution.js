2cc98b563aaaf1ef36be2faf9ad11043b972e61a
dvbrown <dvbrown@student.unimelb.edu.au>
2014-03-10 14:14:07 +1100

    switch attempt

--- calculateRNAdilution.js
+++ calculateRNAdilution.js
@@ -1,11 +1,21 @@
-<script>
-
-function calculate() {
-var RNA_per_rxn = document.getElementById("RNA_per_rxn");
-var dilution = document.getElementById("dilution");
-var total_RNA = document.getElementById("total_RNA");
-
-var dilution = Number(total_RNA) * Number(RNA_per_rxn);
-return dilution;
+<script type='text/javascript' >
+function startCalc(){
+  interval = setInterval("calc()",1);
+}
+function calc(){
+  one = document.autoSumForm.firstBox.value;
+  two = document.autoSumForm.secondBox.value;
+  document.autoSumForm.thirdBox.value = (one * 1) + (two * 1);
+}
+function stopCalc(){
+  clearInterval(interval);
 }
 </script>
+this is the html code
+
+<form name="autoSumForm">
+ 
+<input class="right" type=text name="firstBox" value="" onFocus="startCalc();" onBlur="stopCalc();"><br>
++ <input class="right" type=text name="secondBox" value="" onFocus="startCalc();" onBlur="stopCalc();"><br>
+= <input class="right" type=text name="thirdBox">
+</form>
