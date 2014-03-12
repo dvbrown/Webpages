aa0f5b2ae07ac05d91173e20a99eb0fda625972e
dvbrown <dvbrown@student.unimelb.edu.au>
2014-03-12 15:14:50 +1100

    added menu bar

---
+++ 140314_cellCulturemenuBar.js
@@ -0,0 +1,11 @@
+/*
+Dan Brown used javascript because he couldn't be bothered to write a PHP script and set up a server.
+This script prints a menu on the top of protocol pages. The style NAVMENU is defined in plain.css
+*/
+
+document.write('<div id="navmenu"> \
+<ul> \
+	<li><a href="glioma-stem-cell-culture.html">Gliomasphere culture</a></li> \
+	<li><a href="140312_invasionAssay.html">Spheroid invasion assay</a></li> \
+	<li><a href="140312_resazurinLDH.html">Resazurin/ LDH assay</a></li> \
+</ul></div>');
