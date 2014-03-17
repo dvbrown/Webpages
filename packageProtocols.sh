65a7568f6296aea5ed845951009493bcf0544c0a
dvbrown <dvbrown@student.unimelb.edu.au>
2014-03-17 12:10:08 +1100

    updated protocols added, cell count table

--- packageProtocols.sh
+++ packageProtocols.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 tar -zcvf $1 121016-qRT-PCR.html 130521_RNaseEtOHppt.html 1306* \
-130625_menuBar.js glioma-stem-cell-culture.html pinky.css plain.css
+130625_menuBar.js glioma-stem-cell-culture.html plain.css \
+140312* 140314_cellCulturemenuBar.js /Applications/ImageJ/macros/invasionAssay/*.ijm \
+*.xlsx
